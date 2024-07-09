ee6aa74cf5539654d7bd00ad81dce4791bb96a54
FedericoPetre <federicopetre22@gmail.com>
2024-07-09 18:37:57 -0300

    Commit de reacomodamiento del repo

--- dataBase_HDTech.sql
+++ dataBase_HDTech.sql
@@ -3,38 +3,17 @@ create database hdtech;
 use hdtech;
 
 create table productos(
-	id_producto int (15) primary key auto_increment,
-    src_imagen_producto varchar(400) not null,
-    alt_imagen_producto varchar(80) not null,
-    precio_producto float not null,
-    estado_producto varchar(10) not null,
-	nombre_producto varchar(100) not null,
-    cantidad_producto int);
-    
+	  id_producto INT PRIMARY KEY AUTO_INCREMENT,
+    src_imagen_producto VARCHAR(400) NOT NULL,
+    alt_imagen_producto VARCHAR(400) NOT NULL,
+    precio_producto FLOAT NOT NULL,
+    estado_producto VARCHAR(150) NOT NULL,
+	  nombre_producto VARCHAR(400) NOT NULL,
+    numero_de_serie VARCHAR(400) NOT NULL);
 
-insert into productos (
-						   id_producto, 
-						   src_imagen_producto, 
-                           alt_imagen_producto, 
-						   precio_producto,
-                           estado_producto,
-                           nombre_producto,
-                           cantidad_producto) 
-                            values 
-                          (
-                           123456789, 
-						   "E:\BRUNO\CURSOS, TALLERES Y SEMINARIOS\CODO A CODO\Repo-FullStackPython-CAC\frontend\src\assets\img\products\apple-watch-41mm-series-8-gps-aluminum-case-with-sport-band-sm.png",
-						   'apple-watch-41mm-series-8-gps-aluminum-case-with-sport-band-sm',
-                           240000,
-                           'En stock',
-                           'Apple Watch Series 8 41mm - Aluminum case with Sport Band',
-                           100);
-		
-alter table productos add column id_producto_html varchar(15);
-
-CREATE TABLE `usuarios` (
-  `Nombre` varchar(300) NOT NULL,
-  `Apellido` varchar(300) NOT NULL,
-  `email` varchar(300) NOT NULL,
-  `contraseña` varchar(300) NOT NULL
+CREATE TABLE usuarios (
+  nombre VARCHAR(300) NOT NULL,
+  apellido VARCHAR(300) NOT NULL,
+  email VARCHAR(300) NOT NULL PRIMARY KEY,
+  contraseña VARCHAR(300) NOT NULL
 );
